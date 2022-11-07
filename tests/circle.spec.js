3f136ab2fafbdeb3252cfdc98de24a5ffd741ae5
gregory <gregorysak90@gmail.com>
2022-11-07 16:46:59 -0300

    test/refect: testando se a funcao circle retorna um objeto com as devidas propriedades.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,18 +24,21 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-  });
-  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+  // Teste se o objeto retornado tem 3 propriedades.
+  // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+  // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+  // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+  // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+  it('4.1 - Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
     expect(circle('')).toBeUndefined();
     expect(circle()).toBeUndefined();
     expect(circle('1')).toBeUndefined();
     expect(circle('dez')).toBeUndefined();
   })
+  it('4.2 - Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+    expect(circle(2)).toEqual({ radius: 2, area: 12.56, circumference: 12.56 });
+    expect(circle(5)).toEqual({ radius: 5, area: 78.5, circumference: 31.400000000000002 });
+    expect(circle(0.7)).toEqual({ radius: 0.7, area: 1.5386, circumference: 4.396 });
+    expect(circle(2.4)).toEqual({ radius: 2.4, area: 18.086399999999998, circumference: 15.072 });
+  });
 });
