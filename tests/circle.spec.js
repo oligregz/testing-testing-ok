86160740f0b544e324ad72ecfe47693b9ad6244b
gregory <gregorysak90@gmail.com>
2022-11-07 16:48:37 -0300

    refect

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,7 +24,6 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  // Teste se o objeto retornado tem 3 propriedades.
   // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
   // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
   // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
@@ -35,10 +34,11 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     expect(circle('1')).toBeUndefined();
     expect(circle('dez')).toBeUndefined();
   })
-  it('4.2 - Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
+  it('4.2 - Verifica se ao receber um raio, a função `circle` retorna um objeto contendos os valores esperados e 3 propriedades', () => {
     expect(circle(2)).toEqual({ radius: 2, area: 12.56, circumference: 12.56 });
     expect(circle(5)).toEqual({ radius: 5, area: 78.5, circumference: 31.400000000000002 });
     expect(circle(0.7)).toEqual({ radius: 0.7, area: 1.5386, circumference: 4.396 });
     expect(circle(2.4)).toEqual({ radius: 2.4, area: 18.086399999999998, circumference: 15.072 });
   });
+
 });
