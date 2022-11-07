5e0bfd1992488d452f1715c8b249a790754ee5b0
gregory <gregorysak90@gmail.com>
2022-11-07 16:36:14 -0300

    refect

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,11 +25,6 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    expect(circle('')).toBeUndefined();
-    expect(circle()).toBeUndefined();
-    expect(circle('1')).toBeUndefined();
-    expect(circle('dez')).toBeUndefined();
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
@@ -37,4 +32,10 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
+  it('Teste se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    expect(circle('')).toBeUndefined();
+    expect(circle()).toBeUndefined();
+    expect(circle('1')).toBeUndefined();
+    expect(circle('dez')).toBeUndefined();
+  })
 });
