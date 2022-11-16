92fd60d8bef68301113a56f5d8366de68ef13467
gregory <gregorysak90@gmail.com>
2022-11-16 08:50:06 -0300

    feat: fetchMenu implementeada a createMenu func

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,11 @@
 // que percorre por todos os itens de `objetoRetornado.consumption`, soma o preço deles e retorna o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = () => {
+  let menu = {
+    fetchMenu: () => {},
+  };
+  return menu;
+};
 
 module.exports = createMenu;
